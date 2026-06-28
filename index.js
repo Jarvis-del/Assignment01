190debe1f81ca6b0ded40de0e177535093639807
Prince Kumar Singh <hacker.ps2004@gmail.com>
2026-06-28 17:13:00 +0530

    Implement smooth scrolling for navigation links

---
+++ index.js
@@ -0,0 +1,22 @@
+
+document.querySelectorAll("nav a").forEach(link => {
+
+    link.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        const id = this.getAttribute("href");
+
+        const section = document.querySelector(id);
+
+        if(section){
+
+            section.scrollIntoView({
+                behavior:"smooth"
+            });
+
+        }
+
+    });
+
+});
